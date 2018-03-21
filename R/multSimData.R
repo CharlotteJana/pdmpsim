3120ce7893947f7b125e27f0c76cc28018186981
Charlotte Jana <charlotte.jana@t-online.de>
2018-03-21 17:32:33 +0100

    method 'getMultSimData.multSimCsv'

--- R/multSimData.R
+++ R/multSimData.R
@@ -20,7 +20,7 @@ getMultSimData.multSim <- function(x, times, seeds, discVarNames){
   data <- NULL
   timeIndex <- NULL
   seedIndex <- NULL
-  all.times <- do.call(seq, as.list(x$model@times))
+  all.times <- fromtoby(x$model@times)
   
   if(missing(times) || is.null(times)){
     times <- all.times
@@ -86,28 +86,74 @@ getMultSimData.multSim <- function(x, times, seeds, discVarNames){
   return(data)
 }
 
-#' @rdname getData
+#' @rdname multSimData
 #' @export
-getTimeslice.multSimCsv <- function(x, times){
-  data <- NULL
-  seeds <- x$seeds
+getMultSimData.multSimCsv <- function(x, times, seeds, discVarNames){
+  data <- data.frame(time = numeric(),
+                     seed = numeric(),
+                     type = character(),
+                     variable = character(),
+                     value = numeric(),
+                     stringsAsFactors = FALSE)
+  timeIndex <- NULL
+  seedIndex <- NULL
   all.times <- fromtoby(x$model@times)
-  index <- NULL
-  for(i in times){
-    a <- which(all.times == i)
-    if(length(a) == 0){
-      warning("There are no simulations for time ", i)
-    }
-    else index <- c(index, a)
+  
+  if(missing(times) || is.null(times)){
+    times <- all.times
+    timeIndex <- seq_along(all.times)
   }
-  for(i in index){
-    d <- data.frame(time = rep(all.times[i], length(seeds)), seed = seeds)
-    for(j in seq_along(x$lafList)){
-      d <- cbind(d, x$lafList[[j]][ , i+1])
-    }
-    colnames(d) <- c(colnames(d)[1:2], names(x$model@init))
-    data <- rbind(data, d)
+  if(missing(seeds) || is.null(seeds)){
+    seeds <- x$seeds
+    seedIndex <- seq_along(seeds)
   }
-  attr(data, "class") <- c("timeData", class(data))
+  if(missing(discVarNames)){
+    discVarNames <- NULL
+    for(i in seq_along(x$model@init)){
+      if(length(unique(x$outputList[[1]][, i+1])) < 6) 
+        discVarNames <- cbind(discVarNames, names(init(x$model))[i])
+    }
+  }
+  
+  # select times
+  if(!identical(times, all.times)){
+    for(i in times){
+      a <- which(all.times == i)
+      if(length(a) == 0){
+        warning("There are no simulations for time ", i)
+      }
+      else 
+        timeIndex <- c(timeIndex, a)
+    }
+  }
+  
+  # select seeds
+  if(!identical(seeds, x$seeds)){
+    for(s in seeds){
+      a <- which(x$seeds == s)
+      if(length(a) == 0){
+        warning("There are no simulations for seed ", s)
+      }
+      else
+        seedIndex <- c(seedIndex, a)
+    }
+  }
+  
+  for(n in seq_along(init(x$model))){
+    varName <- names(init(x$model))[n]
+    for(i in seq_along(timeIndex)){
+      for(j in seq_along(seedIndex)){
+        value <- as.numeric(x$lafList[[n]][seedIndex[j], timeIndex[i]+1])
+        new_row <- data.frame(time = all.times[timeIndex[i]],
+                              seed = x$seeds[seedIndex[j]],
+                              type = ifelse(varName %in% discVarNames, 
+                                            "disc", "cont"),
+                              variable = varName,
+                              value = value)
+        data <- rbind(data, new_row)
+      }
+    }
+  }
+  attr(data, "class") <- c("multSimData", class(data))
   return(data)
 }
